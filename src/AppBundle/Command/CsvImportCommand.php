5f4a56220431abf1d0348b9c434e3790c5def1cf
Kim Wistbacka <kim@kulturhuset.fi>
2016-09-09 10:48:36 +0300

    Improved and fixed CSV import command

--- src/AppBundle/Command/CsvImportCommand.php
+++ src/AppBundle/Command/CsvImportCommand.php
@@ -5,24 +5,15 @@ use AppBundle\Command\Common\ImportCommand;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use \Pdo;
-
 use AppBundle\Entity\Registry;
-
-use AppBundle\Entity\Type;
-use AppBundle\Entity\Organization;
-use AppBundle\Entity\Person;
+use AppBundle\Entity\Entry;
+use AppBundle\Entity\Repository\EntryRepository;
 
 use AppBundle\Entity\Address;
-use AppBundle\Entity\Directory;
-
-use AppBundle\Entity\PropertyGroup;
 use AppBundle\Entity\Property;
 
-use AppBundle\Entity\Status;
 use AppBundle\Entity\Connection;
 use AppBundle\Entity\ConnectionType;
 
@@ -33,246 +24,256 @@ class CsvImportCommand extends ImportCommand
         $this
             ->setName('registry:csv-import')
             ->setDescription('Import CSV data')
-            ->addArgument(
-                'path',
-                InputArgument::REQUIRED,
-                'Path to CSV file'
-            )
-            ->addArgument(
-                'registryID',
-                InputArgument::REQUIRED,
-                'Registry ID'
-            )
-            ->addArgument(
-                'parentEntryID',
-                InputArgument::OPTIONAL,
-                'Parent entry ID'
-            )
+            ->addArgument('type', InputArgument::REQUIRED, 'Entry type')
+            ->addArgument('path', InputArgument::REQUIRED, 'Path to CSV file')
+            ->addArgument('registryID', InputArgument::REQUIRED, 'Registry ID')
+            ->addArgument('parentEntryID', InputArgument::OPTIONAL, 'Parent entry ID')
+            ->addArgument('removeChildEntries', InputArgument::OPTIONAL, 'Remove existing child entries')
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(
+        InputInterface $input, 
+        OutputInterface $output
+    ) {
         ini_set('memory_limit','256M');
         extract($input->getArguments());
 
-        if (!file_exists($path)) {
-            $output->writeln("File:{$path} was not found.");
+        $entryRepository = $this->getManager()->getRepository(
+            'AppBundle:Entry'
+        )->getMappedRepository($type);
+        if (!isset($entryRepository)) {
+            $output->writeln("Entry type:{$type} was not found.");
             return;
         }
-        $file = file_get_contents($path);
-
-        $registry = $this->getManager()->find(
-            'AppBundle\Entity\Registry',
-            $registryID
-        );
+        if (!file_exists($path)) {
+            $output->writeln("CSV file:{$path} was not found.");
+            return;
+        }
+        $registry = $this->getRegistry($registryID);
         if (!$registry) {
             $output->writeln("Registry ID:{$registryID} was not found.");
             return;
         }
-
         $parentEntry = null;
         if (isset($parentEntryID)) {
-            $parentEntry = $this->getManager()->find(
-                'AppBundle\Entity\Entry',
-                $parentEntryID
-            );
+            $parentEntry = $this->getEntry($parentEntryID);
             if (!$parentEntry) {
-                $output->writeln("Entry ID:{$parentEntryID} was not found.");
+                $output->writeln("Parent entry ID:{$parentEntryID} was not found.");
                 return;
             }
+            if (isset($removeChildEntries)
+                && filter_var($removeChildEntries, FILTER_VALIDATE_BOOLEAN)) {
+                $this->removeChildEntries($output, $entryRepository, $registry, $parentEntry);
+            }
         }
+        $this->importCsvFile($output, $entryRepository, $path, $registry, $parentEntry);
+    }
+
+
+
+    protected function getEntry(int $id)
+    {
+        return $this->getManager()->find('AppBundle:Entry', $id);
+    }
+
+    protected function getRegistry(int $id)
+    {
+        return $this->getManager()->find('AppBundle:Registry', $id);
+    }
+
+    protected function removeChildEntries(
+        OutputInterface $output,
+        EntryRepository $entryRepository,
+        Registry $registry,
+        Entry $parentEntry
+    ) {
+        $em = $this->getManager();
+
+        $qb = $em->createQueryBuilder();
+        $qb->select('entry')->from($entryRepository->getClassName(), 'entry');
+        $qb->innerJoin('entry.parentConnections', 'parentConnection');
+        $qb->andWhere(
+            $qb->expr()->eq('entry.registry', ':registry')
+        );
+        $qb->setParameter('registry', $registry);
+        $qb->andWhere(
+            $qb->expr()->in('parentConnection.parentEntry', ':parentEntry')
+        );
+        $qb->setParameter('parentEntry', $parentEntry);
+
+        $result = $qb->getQuery()->getResult();
+        foreach ($result as $entry) {
+            $em->remove($entry);
+        }
+        $em->flush();
+        $em->clear($entryRepository->getClassName());
+        $output->writeln("Deleted " . count($result) . " existing child entries.");
+    }
+
+    protected function importCsvFile(
+        OutputInterface $output,
+        EntryRepository $entryRepository,
+        string $path,
+        Registry $registry,
+        Entry $parentEntry = null
+    ) {
+        $em = $this->getManager();
+        $validator = $this->getValidator();
 
         $output->writeln(
             "Importing CSV file:{$path}"
-            . " into registry ID:{$registryID}"
+            . " into registry ID:" . $registry->getId()
             . (
-                isset($parentEntryID)
-                ? " under entry ID:{$parentEntryID}"
+                isset($parentEntry)
+                ? " with parent entry ID:" . $parentEntry->getId()
                 : null
             )
         );
 
-        $this->import(
-            $output,
-            $registry,
-            $parentEntry,
-            $file
-        );
-    }
-
-    protected function import(
-        OutputInterface $output,
-        Registry $registry,
-        $parentEntry,
-        $file
-    ) {
-        $childType = $this->getType([
-            'class' => Type::CLASS_PERSON,
-            'name' => 'Medlem',
-            'registry' => $registry,
-        ]);
-        if (isset($parentEntry)) {
-            $parentType = $parentEntry->getType();
-            if ($parentType->getName() == 'Förening') {
-                $membership = $this->getConnectionType([
-                    'name' => 'Föreningsmedlemskap',
-                    'parentType' => $parentType,
-                    'childType' => $childType,
-                    'registry' => $registry,
-                ]);
-            } elseif ($parentType->getName() == 'Förbund') {
-                $membership = $this->getConnectionType([
-                    'name' => 'Förbundsmedlemskap',
-                    'parentType' => $parentType,
-                    'childType' => $childType,
-                    'registry' => $registry,
-                ]);
-            }
+        $file = file($path);
+        $labels = array_map('trim', str_getcsv(array_shift($file)));
+        $fields = array_map('trim', str_getcsv(array_shift($file)));
+        $schema = [];
+        foreach ($fields as $key => $value) {
+            list($entity, $field) = explode(':', $value);
+            $schema[$entity][$field] = $key;
         }
 
-        $em = $this->getManager();
-        $validator = $this->getValidator();
-        $output->writeln("Importing members...");
-        $count = 0;
+        $membership = null;
+        if (isset($parentEntry)) {
+            $parentType = $em->getRepository(get_class($parentEntry))->getType();
+            $membership = $this->getConnectionType([
+                'parentType' => $parentType,
+                'childType' => $entryRepository->getType(),
+                'registry' => $registry,
+            ]);
+        }
 
-        foreach ($file as $row) {
-            list(
-                // Member
-                $firstName,
-                $lastName,
-                $gender,
-                $birthYear,
-                $birthMonth,
-                $birthDay,
-                $externalId,
-                $notes,
-                $createdAt,
-                // Address
-                $street,
-                $postalCode,
-                $town,
-                $country,
-                $phone,
-                $mobile,
-                $email
-            ) = array_map('trim', str_getcsv($row));
+        $addressRepository = $em->getRepository('AppBundle:Address');
 
-            // Member
-            $member = new Person();
-            $member
-                ->setRegistry($registry)
-                ->setType($childType)
-                ->setFirstName(mb_convert_case($firstName, MB_CASE_TITLE))
-                ->setLastName(mb_convert_case($lastName, MB_CASE_TITLE));
+        $imported = $count = 0;
+        foreach ($file as $key => $row) {
+            $values = array_map('trim', str_getcsv($row));
+            $errors = [];
 
-            if ($gender == Person::GENDER_MALE
-                || $gender == Person::GENDER_FEMALE) {
-                $member->setGender($gender);
+            // Entry
+            $entryData = array_filter(
+                array_combine(
+                    array_keys($schema['entry']),
+                    array_intersect_key(
+                        $values, 
+                        array_flip($schema['entry'])
+                    )
+                )
+            );
+            if (!empty($entryData['firstName'])) {
+                $entryData['firstName'] = mb_convert_case(
+                    $entryData['firstName'],
+                    MB_CASE_TITLE
+                );
             }
-            $birthYear = abs((int)$birthYear);
-            if ($birthYear) {
-                $member->setBirthYear($birthYear > 9999 ? 9999 : $birthYear);
-            }
-            $birthMonth = abs((int)$birthMonth);
-            if ($birthMonth) {
-                $member->setBirthMonth($birthMonth > 12 ? 12 : $birthMonth);
-            }
-            $birthDay = abs((int)$birthDay);
-            if ($birthDay) {
-                $member->setBirthDay($birthDay > 31 ? 31 : $birthDay);
-            }
-            if (!empty($externalId)) {
-                $member->setExternalId($externalId);
-            }
-            if (!empty($notes)) {
-                $member->setNotes($notes);
-            }
-            if (!empty($createdAt)) {
-                $member->setCreatedAt(new \DateTime($createdAt));
+            if (!empty($entryData['lastName'])) {
+                $entryData['lastName'] = mb_convert_case(
+                    $entryData['lastName'],
+                    MB_CASE_TITLE
+                );
             }
 
-            $errors = $validator->validate($member);
-            if (count($errors)) {
-                $output->writeln("Validation failed for member:");
-                $output->writeln(var_export($row) . PHP_EOL);
-                $output->writeln((string)$errors);
-                continue;
+            $entityName = $entryRepository->getClassName();
+            $entry = new $entityName();
+            $entry->setRegistry($registry);
+            if (isset($schema['property'])) {
+                $propertyData = array_filter(
+                    array_combine(
+                        array_keys($schema['property']),
+                        array_intersect_key(
+                            $values, 
+                            array_flip($schema['property'])
+                        )
+                    )
+                );
+                foreach ($propertyData as $id => $value) {
+                    $entry->addProperty(
+                        $em->find('AppBundle:Property', (int)$id)
+                    );
+                }
             }
-            $em->persist($member);
+            $messages = [];
+            if (!$entryRepository->prepare($entry, $entryData, null, $messages)) {
+                $errors['entry'] = $messages;
+            }
+            $em->persist($entry);
 
             // Address
-            $address = new Address();
-            $address->setClass(Address::CLASS_PRIMARY);
-            $address->setEntry($member);
-            if (!empty($street)) {
-                $address->setStreet(
-                    mb_convert_case($street, MB_CASE_TITLE)
+            if (isset($schema['address'])) {
+                $addressData = array_filter(
+                    array_combine(
+                        array_keys($schema['address']),
+                        array_intersect_key(
+                            $values, 
+                            array_flip($schema['address'])
+                        )
+                    )
                 );
-            }
-            if (!empty($postalCode)) {
-                $address->setPostalCode($postalCode);
-            }
-            if (!empty($town)) {
-                $address->setTown(
-                    mb_convert_case($town, MB_CASE_TITLE)
-                );
-            }
-            if (!empty($country)) {
-                $address->setCountry($country);
-            }
-            if (!empty($phone)) {
-                $address->setPhone($phone);
-            }
-            if (!empty($mobile)) {
-                $address->setMobile($mobile);
-            }
-            if (!empty($email)) {
-                $address->setEmail($email);
+                if (!empty($addressData['street'])) {
+                    $addressData['street'] = mb_convert_case(
+                        $addressData['street'],
+                        MB_CASE_TITLE
+                    );
+                }
+                if (!empty($addressData['town'])) {
+                    $addressData['town'] = mb_convert_case(
+                        $addressData['town'],
+                        MB_CASE_TITLE
+                    );
+                }
+                $address = new Address();
+                $address->setClass(Address::CLASS_PRIMARY);
+                $address->setEntry($entry);
+                $messages = [];
+                if (!$addressRepository->prepare($address, $addressData, null, $messages)) {
+                    $errors['address'] = $messages;
+                }
+                $em->persist($address);
             }
 
-            $errors = $validator->validate($address);
-            if (count($errors)) {
-                $output->writeln("Validation failed for member address:");
-                $output->writeln(var_export($row) . PHP_EOL);
-                $output->writeln((string)$errors);
+            if (!empty($errors)) {
+                $output->writeln("Validation failed for row " . ($key + 3) . ":");
+                foreach ($errors as $entity => $messages) {
+                    foreach ($messages as $field => $message) {
+                        $output->writeln("{$entity}:{$field} {$message}");
+                    }
+                }
+                $em->remove($entry);
+                if (isset($address)) {
+                    $em->remove($address);
+                }
                 continue;
-            }
-            $em->persist($address);
-
-            if (isset($membership)) {
+            } elseif (isset($membership)) {
                 // Connection
                 $connection = new Connection();
                 $connection
                     ->setConnectionType($membership)
                     ->setParentEntry($parentEntry)
-                    ->setChildEntry($member);
-
-                $errors = $validator->validate($connection);
-                if (count($errors)) {
-                    $output->writeln("Validation failed for connection:");
-                    $output->writeln(var_export($row) . PHP_EOL);
-                    $output->writeln((string)$errors);
-                    continue;
-                }
+                    ->setChildEntry($entry);
                 $em->persist($connection);
             }
 
             if (++$count >= 100) {
                 $count = 0;
                 $em->flush();
-                $em->clear('AppBundle\Entity\Person');
-                $em->clear('AppBundle\Entity\Address');
-                $em->clear('AppBundle\Entity\Connection');
+                $em->clear($entryRepository->getClassName());
+                $em->clear('AppBundle:Address');
+                $em->clear('AppBundle:Connection');
             }
+            $imported++;
         }
         $em->flush();
-        $em->clear('AppBundle\Entity\Person');
-        $em->clear('AppBundle\Entity\Address');
-        $em->clear('AppBundle\Entity\Connection');
+        $em->clear($entryRepository->getClassName());
+        $em->clear('AppBundle:Address');
+        $em->clear('AppBundle:Connection');
 
-        $output->writeln(count($members) . " members imported.");
-
-        return $members;
+        $output->writeln("{$imported} members imported.");
     }
 }
